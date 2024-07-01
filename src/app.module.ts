b03f4a32dc14331c66e9b0c0f7765e92625f2982
afsnot15 <afsnot15@gmail.com>
2024-07-01 14:50:47 -0300

    ajuste configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './config/database/database.module';
+import { ProdutoModule } from './core/produto/produto.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { DatabaseModule } from './config/database/database.module';
       isGlobal: true,
     }),
     DatabaseModule,
+    ProdutoModule,
   ],
   controllers: [AppController],
   providers: [AppService],
